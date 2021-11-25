181d8bd8bae689188ab5bde43ec64fbf32d299a3
RishiSD <rishideorukhkar@gmail.com>
2021-11-25 13:46:26 +0530

    add cred

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_zz45h0iDo', // e.g. us-east-1_zz45h0iDo
-        userPoolClientId: 'i3sdft7vqr03283cetb45rlr970', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '3sdft7vqr03283cetb45rlr970', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
